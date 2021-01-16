673a5663ef47a7441ce07fba0a17072e71a08c5b
SamKlep <shamdemann23@gmail.com>
2021-01-16 10:52:18 -0600

    OrderScreen.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
 
 const App = () => {
   return (
@@ -20,12 +21,13 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/login' component={LoginScreen} exact />
-          <Route path='/shipping' component={ShippingScreen} exact />
-          <Route path='/payment' component={PaymentScreen} exact />
-          <Route path='/placeorder' component={PlaceOrderScreen} exact />
-          <Route path='/register' component={RegisterScreen} exact />
-          <Route path='/profile' component={ProfileScreen} exact />
+          <Route path='/order/:id' component={OrderScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
