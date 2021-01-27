38531a5bf06592da3b9056b7e4c4b21fe92ab435
SamKlep <shamdemann23@gmail.com>
2021-01-27 16:20:16 -0600

    Image upload - frontend.

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,19 +1,22 @@
+import path from 'path'
 import express from 'express'
 import multer from 'multer'
-import path from 'path'
 const router = express.Router()
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, '/uploads/')
+    cb(null, 'uploads/')
   },
   filename(req, file, cb) {
-    cb(null, `${file.filename}-${Date.now()}${path.extname(file.originalname)}`)
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    )
   },
 })
 
 function checkFileType(file, cb) {
-  const fileTypes = /jpg|jpeg|png/
+  const filetypes = /jpg|jpeg|png/
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
   const mimetype = filetypes.test(file.mimetype)
 
